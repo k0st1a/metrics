f99aca0da7f694073e3f3099b459ac99ad4efa51
konstantin malikov <kostiamalikov@gmail.com>
2023-11-13 00:47:20 +0700

    Код агента перенесен в internal

---
+++ internal/agent/agent.go
@@ -0,0 +1,17 @@
+package agent
+
+import (
+	"github.com/k0st1a/metrics/internal/agent/client"
+	"github.com/k0st1a/metrics/internal/agent/metrics"
+	"net/http"
+)
+
+func Run() {
+	var myMetrics *metrics.MyStats = &metrics.MyStats{}
+	var pollInternal = 2
+	var reportInterval = 10
+	var myClient *http.Client = &http.Client{}
+
+	go metrics.RunUpdateMetrics(myMetrics, pollInternal)
+	client.RunReportMetrics(myClient, myMetrics, reportInterval)
+}
---
+++ internal/agent/client/client.go
@@ -0,0 +1,41 @@
+package client
+
+import (
+	"github.com/k0st1a/metrics/internal/agent/metrics"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func RunReportMetrics(client *http.Client, metrics *metrics.MyStats, reportInterval int) {
+	for {
+		time.Sleep(time.Duration(reportInterval) * time.Second)
+		reportMetrics(client, metrics)
+	}
+}
+
+func reportMetrics(client *http.Client, metrics *metrics.MyStats) {
+	prepared := metrics.Prepare()
+	for name, info := range prepared {
+		ReportMetric(client, info.Type, name, info.Value)
+	}
+}
+
+func ReportMetric(client *http.Client, metricType, name, value string) {
+	var url string = `http://localhost:8080/update/` + metricType + `/` + name + `/` + value
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "text/plain")
+	req.Header.Set("Content-Length", "0")
+
+	response, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, response.Body) // вывод ответа в консоль
+	response.Body.Close()
+}
