b182cc4d4cf92ca86b60c6d38a97dbe68849dcba
konstantin malikov <kostiamalikov@gmail.com>
2023-11-12 18:36:04 +0700

    Поправлено использование логера

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/k0st1a/metrics/internal/logger"
 	"github.com/k0st1a/metrics/internal/server"
 )
 
 func main() {
+	fmt.Println("Running logger")
 	logger.Run()
+	defer logger.Close()
+
+	fmt.Println("Running server")
 	server.Run()
 }
