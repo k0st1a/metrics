503ad327c7ea26b6e9a534280f4b3ca85e15e22c
konstantin malikov <kostiamalikov@gmail.com>
2023-11-12 18:34:51 +0700

    Поправлена работа логера

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,13 +1,13 @@
 package logger
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 var MyLog *log.Logger
+var MyLogFile *os.File
 
 func LogHTTPRequest(req *http.Request) {
 	MyLog.Println("Method:", req.Method)
@@ -21,18 +21,21 @@ func LogHTTPRequest(req *http.Request) {
 }
 
 func Println(v ...any) {
-	fmt.Println(v...)
+	MyLog.Println(v...)
 }
 
 func Run() {
-	f, err := os.OpenFile("./server.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	MyLogFile, err := os.OpenFile("./server.log", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	MyLog = log.New(f, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile)
+	MyLog = log.New(MyLogFile, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile)
 	//Log = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	MyLog.Println("Log started")
+}
+
+func Close() {
+	MyLogFile.Close()
 }
