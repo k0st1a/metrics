7ff4f8fc1b14d069e28b4e6c9a76d3044abd2034
konstantin malikov <kostiamalikov@gmail.com>
2023-11-14 23:10:36 +0700

    Добавлены пакеты gauge и counter

---
+++ internal/storage/gauge/gauge.go
@@ -0,0 +1,34 @@
+package gauge
+
+import (
+	"strconv"
+
+	"github.com/k0st1a/metrics/internal/storage"
+)
+
+func Store(name, value string) error {
+	v, err := parser(value)
+	if err != nil {
+		return err
+	}
+
+	storage.StoreGauge(name, v)
+	return nil
+}
+
+func Get(name string) (string, bool) {
+	v, ok := storage.GetGauge(name)
+	if !ok {
+		return "", ok
+	}
+
+	return stringer(v), ok
+}
+
+func parser(s string) (float64, error) {
+	return strconv.ParseFloat(s, 64)
+}
+
+func stringer(f float64) string {
+	return strconv.FormatFloat(f, 'g', -1, 64)
+}
