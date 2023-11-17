c4472bf2ee8973de2e04c447731b5243c1cd47f3
konstantin malikov <kostiamalikov@gmail.com>
2023-11-18 02:56:02 +0700

    Поддержаны переменные окружения на агенте

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -2,6 +2,8 @@ package agent
 
 import (
 	"github.com/k0st1a/metrics/internal/agent/report"
+	//"github.com/k0st1a/metrics/internal/logger"
+	"fmt"
 	"github.com/k0st1a/metrics/internal/metrics"
 	"net/http"
 )
@@ -10,8 +12,13 @@ func Run() {
 	var myMetrics = &metrics.MyStats{}
 	var myClient = &http.Client{}
 
-	parseFlags()
+	cfg := NewConfig()
+	fmt.Println("Config:", cfg)
+	parseFlags(&cfg)
+	fmt.Println("Config after parseFlags:", cfg)
+	parseEnv(&cfg)
+	fmt.Println("Config after parseEnv:", cfg)
 
-	go metrics.RunUpdateMetrics(myMetrics, pollInternal)
-	report.RunReportMetrics(serverAddr, myClient, myMetrics, reportInterval)
+	go metrics.RunUpdateMetrics(myMetrics, cfg.PollInterval)
+	report.RunReportMetrics(cfg.ServerAddr, myClient, myMetrics, cfg.ReportInterval)
 }
---
+++ internal/agent/env.go
@@ -0,0 +1,11 @@
+package agent
+
+import "github.com/caarlos0/env/v10"
+
+func parseEnv(cfg *Config) error {
+	err := env.Parse(cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
