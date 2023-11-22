59ba722c3ea5f3809900ba7763287331b235d279
konstantin malikov <kostiamalikov@gmail.com>
2023-11-22 19:34:15 +0700

    Перенос кода получения конфигурации в один файл config.go

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -3,7 +3,6 @@ package agent
 import (
 	"github.com/k0st1a/metrics/internal/agent/report"
 	"github.com/k0st1a/metrics/internal/metrics"
-	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
@@ -19,36 +18,6 @@ func Run() error {
 
 	go metrics.RunUpdateMetrics(myMetrics, cfg.PollInterval)
 	report.RunReportMetrics(cfg.ServerAddr, myClient, myMetrics, cfg.ReportInterval)
-
-	return nil
-}
-
-func collectConfig(cfg *Config) error {
-	log.Debug().
-		Str("cfg.ServerAddr", cfg.ServerAddr).
-		Int("cfg.PollInterval", cfg.PollInterval).
-		Int("cfg.ReportInterval", cfg.ReportInterval).
-		Msg("")
-
-	err := parseFlags(cfg)
-	if err != nil {
-		return err
-	}
-	log.Debug().
-		Str("cfg.ServerAddr", cfg.ServerAddr).
-		Int("cfg.PollInterval", cfg.PollInterval).
-		Int("cfg.ReportInterval", cfg.ReportInterval).
-		Msg("After parseFlags")
-
-	err = parseEnv(cfg)
-	if err != nil {
-		return err
-	}
-	log.Debug().
-		Str("cfg.ServerAddr", cfg.ServerAddr).
-		Int("cfg.PollInterval", cfg.PollInterval).
-		Int("cfg.ReportInterval", cfg.ReportInterval).
-		Msg("After parseEnv")
 
 	return nil
 }
--- internal/agent/config.go
+++ internal/agent/config.go
@@ -1,5 +1,14 @@
 package agent
 
+import (
+	"errors"
+	"flag"
+
+	"github.com/caarlos0/env/v10"
+	"github.com/k0st1a/metrics/internal/utils"
+	"github.com/rs/zerolog/log"
+)
+
 type Config struct {
 	ServerAddr     string `env:"ADDRESS"`
 	PollInterval   int    `env:"POLL_INTERVAL"`
@@ -12,4 +21,64 @@ func NewConfig() Config {
 		PollInterval:   2,
 		ReportInterval: 10,
 	}
+}
+
+func parseEnv(cfg *Config) error {
+	err := env.Parse(cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func parseFlags(cfg *Config) error {
+	addr := &utils.NetAddress{}
+	addr.Set(cfg.ServerAddr)
+
+	// если интерфейс не реализован,
+	// здесь будет ошибка компиляции
+	_ = flag.Value(addr)
+	flag.Var(addr, "a", "server network address in a form host:port")
+
+	flag.IntVar(&(cfg.PollInterval), "p", 2, "metrics polling rate to the server")
+	flag.IntVar(&(cfg.ReportInterval), "r", 10, "frequency of sending metrics to the server")
+
+	flag.Parse()
+	cfg.ServerAddr = addr.String()
+
+	if len(flag.Args()) != 0 {
+		return errors.New("unknown args")
+	}
+
+	return nil
+}
+
+func collectConfig(cfg *Config) error {
+	log.Debug().
+		Str("cfg.ServerAddr", cfg.ServerAddr).
+		Int("cfg.PollInterval", cfg.PollInterval).
+		Int("cfg.ReportInterval", cfg.ReportInterval).
+		Msg("")
+
+	err := parseFlags(cfg)
+	if err != nil {
+		return err
+	}
+	log.Debug().
+		Str("cfg.ServerAddr", cfg.ServerAddr).
+		Int("cfg.PollInterval", cfg.PollInterval).
+		Int("cfg.ReportInterval", cfg.ReportInterval).
+		Msg("After parseFlags")
+
+	err = parseEnv(cfg)
+	if err != nil {
+		return err
+	}
+	log.Debug().
+		Str("cfg.ServerAddr", cfg.ServerAddr).
+		Int("cfg.PollInterval", cfg.PollInterval).
+		Int("cfg.ReportInterval", cfg.ReportInterval).
+		Msg("After parseEnv")
+
+	return nil
 }
