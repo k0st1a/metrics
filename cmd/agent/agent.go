51eb1190ec313b88dfe873f1e1ee38a08119762a
konstantin malikov <kostiamalikov@gmail.com>
2023-11-22 18:20:24 +0700

    Исправления замечаний по Code Review
    1. Использование логирования из zerolog
    2. Cбор конфигурации из флогов и переменных вынесен в отдельную функцию collectConfig
    3. Прокидывание ошибок парсинга флагов и переменных окружения до самого
    верхнего уровня с их последущих вывод в лог

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/k0st1a/metrics/internal/agent"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	agent.Run()
+	err := agent.Run()
+	if err != nil {
+		log.Error().Err(err)
+	}
 }
