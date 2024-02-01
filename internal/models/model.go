e96ececc021b2f82301340862e32db45dd8319d2
konstantin malikov <kostiamalikov@gmail.com>
2024-02-02 01:18:13 +0700

    Правки после запуску golangci-lint

--- internal/models/model.go
+++ internal/models/model.go
@@ -8,10 +8,10 @@ import (
 
 //go:generate easyjson -all model.go
 type Metrics struct {
-	ID    string   `json:"id"`              // имя метрики
-	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
 	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
 }
 
 func Deserialize(b []byte) (*Metrics, error) {
