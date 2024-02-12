ec5c5cf6ab01ee348a513cc82924e571ce5e87f5
konstantin malikov <kostiamalikov@gmail.com>
2024-02-12 22:44:42 +0700

    Реализация Ping до PosgreSQL

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 
 	"github.com/k0st1a/metrics/internal/handlers"
+	dbh "github.com/k0st1a/metrics/internal/handlers/db"
 	"github.com/k0st1a/metrics/internal/handlers/json"
 	"github.com/k0st1a/metrics/internal/handlers/text"
+	dbs "github.com/k0st1a/metrics/internal/storage/db"
 	"github.com/k0st1a/metrics/internal/storage/file"
 	"github.com/k0st1a/metrics/internal/storage/inmemory"
 	"github.com/rs/zerolog/log"
@@ -22,6 +24,10 @@ type Storage interface {
 	GetAll() (map[string]int64, map[string]float64)
 }
 
+type Pinger interface {
+	Ping() error
+}
+
 func Run() error {
 	log.Debug().Msg("Run server")
 
@@ -32,21 +38,29 @@ func Run() error {
 
 	printConfig(cfg)
 
+	var p Pinger
+	if cfg.DatabaseDSN != "" {
+		p = dbs.NewStorage(cfg.DatabaseDSN)
+	}
+
 	var s Storage
-	if cfg.FileStoragePath == "" {
-		log.Debug().Msg("Using memory storage")
-		s = inmemory.NewStorage()
-	} else {
+	switch {
+	case cfg.FileStoragePath != "":
 		log.Debug().Msg("Using file storage")
 		s = file.NewStorage(cfg.FileStoragePath, cfg.StoreInterval, cfg.Restore)
+	default:
+		log.Debug().Msg("Using memory storage")
+		s = inmemory.NewStorage()
 	}
 
 	th := text.NewHandler(s)
 	jh := json.NewHandler(s)
+	dh := dbh.NewHandler(p)
 
 	r := handlers.NewRouter()
 	text.BuildRouter(r, th)
 	json.BuildRouter(r, jh)
+	dbh.BuildRouter(r, dh)
 
 	err = http.ListenAndServe(cfg.ServerAddr, r)
 	if err != nil {
---
+++ internal/storage/db/storage.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog/log"
+)
+
+type Pinger interface {
+	Ping() error
+}
+
+type dbStorage struct {
+	databaseDSN string
+}
+
+func NewStorage(dsn string) Pinger {
+	return &dbStorage{
+		databaseDSN: dsn,
+	}
+}
+
+func (db *dbStorage) Ping() error {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, db.databaseDSN)
+	if err != nil {
+		log.Error().Err(err).Msg("pgx.Connect error")
+		return fmt.Errorf("pgx.Connect error:%w", err)
+	}
+	defer conn.Close(ctx)
+
+	err = conn.Ping(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("pgx.Ping error")
+		return fmt.Errorf("pgx.Ping error:%w", err)
+	}
+
+	return nil
+}
