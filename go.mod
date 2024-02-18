9d489b369b788fc2ee1afb61a0e6e06689fe4590
konstantin malikov <kostiamalikov@gmail.com>
2024-02-18 23:31:38 +0700

    Поддержка db storage

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/caarlos0/env/v10 v10.0.0
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/jackc/pgerrcode v0.0.0-20220416144525-469b46aa5efa
 	github.com/jackc/pgx/v5 v5.5.3
 	github.com/mailru/easyjson v0.7.7
 	github.com/rs/zerolog v1.31.0
@@ -15,6 +16,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -22,6 +24,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/storage/db/storage.go
+++ internal/storage/db/storage.go
@@ -2,45 +2,169 @@ package db
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"sync"
 
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
 )
 
-type Pinger interface {
-	Ping() error
+type Storage interface {
+	StoreGauge(ctx context.Context, name string, value float64) error
+	GetGauge(ctx context.Context, name string) (*float64, error)
+
+	StoreCounter(ctx context.Context, name string, value int64) error
+	GetCounter(ctx context.Context, name string) (*int64, error)
+
+	GetAll(ctx context.Context) (counter map[string]int64, gauge map[string]float64, err error)
 }
 
 type dbStorage struct {
-	databaseDSN string
+	c *pgxpool.Pool
+	m sync.Mutex
 }
 
-func NewStorage(dsn string) Pinger {
+func NewStorage(c *pgxpool.Pool) Storage {
 	return &dbStorage{
-		databaseDSN: dsn,
+		c: c,
 	}
 }
 
-func (db *dbStorage) Ping() error {
-	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, db.databaseDSN)
-	if err != nil {
-		log.Error().Err(err).Msg("pgx.Connect error")
-		return fmt.Errorf("pgx.Connect error:%w", err)
-	}
-	defer func() {
-		err := conn.Close(ctx)
-		if err != nil {
-			log.Error().Err(err).Msg("conn.Close error")
-		}
-	}()
+func (s *dbStorage) StoreGauge(ctx context.Context, name string, value float64) error {
+	s.m.Lock()
+	defer s.m.Unlock()
 
-	err = conn.Ping(ctx)
+	_, err := s.c.Exec(ctx, "INSERT INTO gauges (name,value) VALUES($1, $2) ON CONFLICT (name) DO UPDATE SET delta = delta + $2", name, value)
 	if err != nil {
-		log.Error().Err(err).Msg("pgx.Ping error")
-		return fmt.Errorf("pgx.Ping error:%w", err)
+		return fmt.Errorf("store gauge query error:%w", err)
 	}
 
 	return nil
+}
+
+func (s *dbStorage) GetGauge(ctx context.Context, name string) (*float64, error) {
+	var v float64
+
+	err := s.c.QueryRow(ctx, "SELECT value FROM gauges WHERE name = $1", name).Scan(&v)
+	if err != nil {
+		return nil, fmt.Errorf("get gauge query error:%w", err)
+	}
+
+	return &v, nil
+}
+
+func (s *dbStorage) StoreCounter(ctx context.Context, name string, value int64) error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	tag, err := s.c.Exec(ctx, "INSERT INTO counters (name,delta) VALUES($1, $2) ON CONFLICT (name) DO UPDATE SET delta = $2", name, value)
+	log.Printf("tag:%v, err:%v", tag, err)
+	if err != nil {
+		return fmt.Errorf("store counter query error:%w", err)
+	}
+
+	return nil
+}
+
+func (s *dbStorage) GetCounter(ctx context.Context, name string) (*int64, error) {
+	log.Printf("GetCounter, name:%v", name)
+	var d int64
+	log.Printf("d:%v", d)
+
+	err := s.c.QueryRow(ctx, "SELECT delta FROM counters WHERE name = $1", name).Scan(&d)
+	log.Printf("err:%+v", err)
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, nil
+	}
+
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgerrcode.IsNoData(pgErr.Code) {
+			return nil, nil
+		}
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("get counter query error:%w", err)
+	}
+	log.Printf("d:%v", d)
+
+	return &d, nil
+}
+
+func (s *dbStorage) GetAll(ctx context.Context) (map[string]int64, map[string]float64, error) {
+	c, err := s.getCounters(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	g, err := s.getGauges(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return c, g, nil
+}
+
+func (s *dbStorage) getGauges(ctx context.Context) (map[string]float64, error) {
+	rows, err := s.c.Query(ctx, "SELECT name,value FROM gauges")
+	if err != nil {
+		return nil, fmt.Errorf("get gauges query error:%w", err)
+	}
+	defer rows.Close()
+
+	g := make(map[string]float64)
+
+	for rows.Next() {
+		var name string
+		var value float64
+
+		err = rows.Scan(&name, &value)
+		if err != nil {
+			return nil, fmt.Errorf("rows scan error:%w", err)
+		}
+
+		g[name] = value
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("rows error:%w", err)
+	}
+
+	return g, nil
+}
+
+func (s *dbStorage) getCounters(ctx context.Context) (map[string]int64, error) {
+	rows, err := s.c.Query(ctx, "SELECT name,delta FROM counters")
+	if err != nil {
+		return nil, fmt.Errorf("get counters query error:%w", err)
+	}
+	defer rows.Close()
+
+	c := make(map[string]int64)
+
+	for rows.Next() {
+		var name string
+		var delta int64
+
+		err = rows.Scan(&name, &delta)
+		if err != nil {
+			return nil, fmt.Errorf("rows scan error:%w", err)
+		}
+
+		c[name] = delta
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("rows error:%w", err)
+	}
+
+	return c, nil
 }
