1b92e325883849a52a282a6dc41449793c8b3aa3
konstantin malikov <kostiamalikov@gmail.com>
2024-03-17 22:58:45 +0700

    Добавлен закомментированный код по включению расширенного логера

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+	//log.Logger = log.With().Caller().Logger()
+	//name := fmt.Sprintf("./myapp-%v.log", time.Now().String())
+	//log.Printf("Log to file:%v", name)
+	//file, _ := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+	//log.Logger = zerolog.New(file).With().Caller().Timestamp().Logger()
+
 	err := server.Run()
 	if err != nil {
 		log.Fatal().Err(err).Msg("")
