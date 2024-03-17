5540b13cbf0e0e295618b84e51b429d342096491
konstantin malikov <kostiamalikov@gmail.com>
2024-03-17 22:45:25 +0700

    Поддержан новый хендлер POST /updates/

--- internal/handlers/json/handlers.go
+++ internal/handlers/json/handlers.go
@@ -24,6 +24,9 @@ type Storage interface {
 
 	GetCounter(ctx context.Context, name string) (*int64, error)
 	StoreCounter(ctx context.Context, name string, value int64) error
+
+	StoreAll(ctx context.Context, counter map[string]int64, gauge map[string]float64) error
+	GetAll(ctx context.Context) (counter map[string]int64, gauge map[string]float64, err error)
 }
 
 type handler struct {
@@ -37,8 +40,81 @@ func NewHandler(s Storage) *handler {
 }
 
 func BuildRouter(r *chi.Mux, h *handler) {
+	r.Post("/updates/", h.PostUpdatesHandler)
 	r.Post("/update/", h.PostUpdateHandler)
 	r.Post("/value/", h.PostValueHandler)
+}
+
+func (h *handler) PostUpdatesHandler(rw http.ResponseWriter, r *http.Request) {
+	log.Info().
+		Str("uri", r.RequestURI).
+		Str("method", r.Method).
+		Msg("")
+
+	ct := r.Header.Get("Content-Type")
+	if ct != "application/json" {
+		http.Error(rw, badContentType, http.StatusBadRequest)
+		return
+	}
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Error().Err(err).Msg("io.ReadAll error")
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if err != nil {
+			log.Error().Err(err).Msg("")
+		}
+	}()
+
+	m, err := models.DeserializeList(b)
+	if err != nil {
+		log.Error().Err(err).Msg("models.Deserialize error")
+		http.Error(rw, "deserialize error", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+
+	g := make(map[string]float64)
+	c := make(map[string]int64)
+
+	for _, v := range m {
+		switch v.MType {
+		case "counter":
+			cv, ok := c[v.ID]
+			switch ok {
+			case true:
+				c[v.ID] = cv + *v.Delta
+			default:
+				c[v.ID] = *v.Delta
+			}
+		case "gauge":
+			g[v.ID] = *v.Value
+		default:
+			log.Error().
+				Str("unknown MType", v.MType).
+				Msg("")
+		}
+	}
+
+	log.Printf("Store\nCounters:%+v\nGauges:%+v\n", c, g)
+
+	err = h.storage.StoreAll(ctx, c, g)
+	if err != nil {
+
+		log.Error().Err(err).Msg("s.StoreAll error")
+		rw.WriteHeader(http.StatusInternalServerError)
+
+		//c, g, err = h.storage.GetAll(ctx)
+		//log.Printf("c:%v, g:%v, err:%v", c, g, err)
+
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
 }
 
 func (h *handler) PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
@@ -78,9 +154,19 @@ func (h *handler) PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
 	case "counter":
 		log.Printf("Post Update counter, name(%v), value(%v)", m.ID, *m.Delta)
 		AddCounter(ctx, h.storage, m.ID, *m.Delta)
+		if err != nil {
+			log.Error().Err(err).Msg("add counter error")
+			http.Error(rw, "store counter error", http.StatusInternalServerError)
+			return
+		}
 	case "gauge":
 		log.Printf("Post Update gauge, name(%v), value(%v)", m.ID, *m.Value)
-		h.storage.StoreGauge(ctx, m.ID, *m.Value)
+		err = h.storage.StoreGauge(ctx, m.ID, *m.Value)
+		if err != nil {
+			log.Error().Err(err).Msg("h.storage.StorageGauge")
+			http.Error(rw, "storage gauge error", http.StatusInternalServerError)
+			return
+		}
 	default:
 		http.Error(rw, badMetricType, http.StatusBadRequest)
 		return
