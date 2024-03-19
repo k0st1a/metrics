104c0de711772e559c085d8cf8b4dc34bb047e14
konstantin malikov <kostiamalikov@gmail.com>
2024-03-20 02:04:24 +0700

    Рефакторинг: приведение к общему виду

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,13 +3,12 @@ package handlers
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/k0st1a/metrics/internal/middleware"
-	"github.com/k0st1a/metrics/internal/middleware/logging"
 )
 
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Use(logging.Logging)
+	r.Use(middleware.Logging)
 	r.Use(middleware.Compress)
 
 	return r
