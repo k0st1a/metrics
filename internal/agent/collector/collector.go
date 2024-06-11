90c9454aeed9c61cd46c20b0df7c207c5265952c
konstantin malikov <kostiamalikov@gmail.com>
2024-06-11 21:25:03 +0700

    Рефакторинг: небольшой рефакторинг

--- internal/agent/collector/collector.go
+++ internal/agent/collector/collector.go
@@ -8,17 +8,13 @@ type MetricInfoRawer interface {
 	MetricInfoRaw() []model.MetricInfoRaw
 }
 
-type Doer interface {
-	Do()
-}
-
 type state struct {
 	in     <-chan struct{}
 	out    chan<- []model.MetricInfoRaw
 	metric MetricInfoRawer
 }
 
-func NewCollector(in <-chan struct{}, m MetricInfoRawer) (Doer, <-chan []model.MetricInfoRaw) {
+func NewCollector(in <-chan struct{}, m MetricInfoRawer) (*state, <-chan []model.MetricInfoRaw) {
 	out := make(chan []model.MetricInfoRaw)
 	return &state{
 		in:     in,
