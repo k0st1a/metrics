a766a8473ef32d0a9354921ae229f162a1c073ab
konstantin malikov <kostiamalikov@gmail.com>
2024-06-16 01:50:33 +0700

    Убран golang.org/x/tools/go/analysis/passes/shadow за ненадобностью

--- internal/pkg/analyzer/multichecker/main.go
+++ internal/pkg/analyzer/multichecker/main.go
@@ -5,7 +5,6 @@ import (
 
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/printf"
-	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/shift"
 	"golang.org/x/tools/go/analysis/passes/structtag"
 )
@@ -13,7 +12,6 @@ import (
 func Run() {
 	multichecker.Main(
 		printf.Analyzer,
-		shadow.Analyzer,
 		shift.Analyzer,
 		structtag.Analyzer,
 		osexit.Analyzer,
