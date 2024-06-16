651d2e3cb9ed9498506e7702f1758764792222d5
konstantin malikov <kostiamalikov@gmail.com>
2024-06-17 02:17:02 +0700

    Для покрытия тестами добавлен тест для internal/middleware/compress

---
+++ internal/middleware/compress_test.go
@@ -0,0 +1,113 @@
+package middleware
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMiddlewareCompress(t *testing.T) {
+	tests := []struct {
+		name                    string
+		acceptEncoding          string
+		path                    string
+		body                    io.Reader
+		expectedContentEncoding string
+	}{
+		{
+			name:                    "check compress application/json",
+			acceptEncoding:          "gzip",
+			path:                    "/get_application_json",
+			expectedContentEncoding: "gzip",
+		},
+		{
+			name:                    "check compress text/html",
+			acceptEncoding:          "gzip",
+			path:                    "/get_text_html",
+			expectedContentEncoding: "gzip",
+		},
+		{
+			name:                    "check no compress for no Accept-Encoding",
+			acceptEncoding:          "",
+			path:                    "/get_application_json",
+			expectedContentEncoding: "",
+		},
+		{
+			name:                    "check no compress for unknown Content-Type",
+			acceptEncoding:          "gzip",
+			path:                    "/get_unknown_content_type",
+			expectedContentEncoding: "",
+		},
+	}
+
+	r := chi.NewRouter()
+	r.Use(Compress)
+
+	r.Get("/get_application_json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("textstring"))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	r.Get("/get_text_html", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("textstring"))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	r.Get("/get_unknown_content_type", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "unknown")
+		w.Write([]byte("textstring"))
+		w.WriteHeader(http.StatusOK)
+	})
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	tc := &http.Client{
+		Transport: &http.Transport{
+			DisableCompression: true,
+		}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, ts.URL+test.path, nil)
+			assert.NoError(t, err)
+
+			req.Header.Set("Accept-Encoding", test.acceptEncoding)
+			assert.NoError(t, err)
+
+			resp, err := tc.Do(req)
+			assert.NoError(t, err)
+			defer resp.Body.Close()
+
+			var reader io.ReadCloser
+
+			switch resp.Header.Get("Content-Encoding") {
+			case "gzip":
+				var err error
+				reader, err = gzip.NewReader(resp.Body)
+				assert.NoError(t, err)
+			default:
+				reader = resp.Body
+			}
+
+			respBody, err := io.ReadAll(reader)
+			assert.NoError(t, err)
+
+			reader.Close()
+
+			assert.Equal(t, "textstring", string(respBody),
+				"response text doesn't match; expected:%q, got:%q", "textstring", string(respBody))
+
+			respContentEncoding := resp.Header.Get("Content-Encoding")
+			assert.Equal(t, test.expectedContentEncoding, respContentEncoding,
+				"expected Content-Encoding %q but got %q", test.expectedContentEncoding, respContentEncoding)
+		})
+	}
+}
