e134a4a6f72edb7260595201b00a5f8f437cc16e
konstantin malikov <kostiamalikov@gmail.com>
2024-07-07 16:14:29 +0700

    Дописывание обработчика. Part 3

--- internal/adapters/api/grpc/server/handler/main.go
+++ internal/adapters/api/grpc/server/handler/main.go
@@ -9,6 +9,8 @@ import (
 	"google.golang.org/grpc/status"
 
 	pb "github.com/k0st1a/metrics/internal/adapters/api/grpc/protobuf"
+	// TODO: Обернуть? Чтобы в http и grpc использовалась одинаковая функция?
+	"github.com/k0st1a/metrics/internal/pkg/retry"
 	"github.com/k0st1a/metrics/internal/ports"
 	"github.com/rs/zerolog/log"
 )
@@ -18,6 +20,7 @@ type MetricsServer struct {
 	// для совместимости с будущими версиями
 	pb.UnimplementedMetricsServer
 	Storage ports.Storage
+	Retry   ports.Retryer
 }
 
 func (s *MetricsServer) GetMetric(ctx context.Context, r *pb.GetMetricRequest) (*pb.GetMetricResponse, error) {
@@ -70,8 +73,8 @@ func (s *MetricsServer) StoreMetric(ctx context.Context, r *pb.StoreMetricReques
 	switch r.Metric.Type {
 	case pb.Type_GAUGE:
 		log.Printf("Store gauge, name(%v), value(%v)", r.Metric.Id, r.Metric.Value)
-		err = h.retry.Retry(ctx, retry.IsConnectionException, func() error {
-			return h.storage.StoreGauge(ctx, m.Metric.Id, r.Metric.Value)
+		err := s.Retry.Retry(ctx, retry.IsConnectionException, func() error {
+			return s.Storage.StoreGauge(ctx, r.Metric.Id, r.Metric.Value)
 		})
 		if err != nil {
 			log.Error().Err(err).Msg("h.storage.StorageGauge error")
@@ -79,16 +82,16 @@ func (s *MetricsServer) StoreMetric(ctx context.Context, r *pb.StoreMetricReques
 		}
 	case pb.Type_COUNTER:
 		log.Printf("Store counter, name(%v), value(%v)", r.Metric.Id, r.Metric.Delta)
-		err = h.retry.Retry(ctx, retry.IsConnectionException, func() error {
+		err := s.Retry.Retry(ctx, retry.IsConnectionException, func() error {
 			//nolint // Не за чем оборачивать ошибку
-			return h.storage.StoreCounter(ctx, m.ID, *m.Delta)
+			return s.Storage.StoreCounter(ctx, r.Metric.Id, r.Metric.Delta)
 		})
 		if err != nil {
 			log.Error().Err(err).Msg("h.storage.StoreCounter error")
 			return nil, status.Errorf(codes.Internal, "for counter %v error:%v", r.Metric.Id, err)
 		}
 	default:
-		return nil, status.Errorf(codes.InvalidArgument, "unknown metric type:%v", r.Type)
+		return nil, status.Errorf(codes.InvalidArgument, "unknown metric type:%v", r.Metric.Type)
 	}
 
 	return &pb.StoreMetricResponse{}, nil
